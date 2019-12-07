a11cb62c9d69a753aa0086b1a79bb3b71432846d
Avinash Kumar <iavinas@yandex.com>
2019-12-07 23:02:39 +0530

    Update 4easier.py

--- LP04/4easier.py
+++ LP04/4easier.py
@@ -15,7 +15,7 @@ synapse_0 = 2*np.random.random((2,hidden_dim)) - 1
 synapse_1 = 2*np.random.random((hidden_dim,1)) - 1
 layer_2 = np.empty_like(y)
 for j in range(60000):
-    layer_1 = np.tanh(np.dot(X,synapse_0))
+    layer_1 = sigmoid(np.dot(X,synapse_0))
     layer_2 = sigmoid(np.dot(layer_1,synapse_1))
     layer_2_delta =  (layer_2 - y) * derivatives_sigmoid(layer_2)
     layer_1_delta = layer_2_delta.dot(synapse_1.T) * derivatives_sigmoid(layer_1)
