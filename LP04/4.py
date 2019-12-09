b69f43a648278ba6eda9c0da15d834ef3726be23
Avinash Kumar <iavinas@yandex.com>
2019-12-09 11:37:35 +0530

    Update 4.py

--- LP04/4.py
+++ LP04/4.py
@@ -2,7 +2,8 @@ import numpy as np
 X = np.array(([2, 9], [1, 5], [3, 6]), dtype=float)
 y = np.array(([92], [86], [89]), dtype=float)
 X = X/np.amax(X,axis=0) # maximum of X array longitudinally
-y = y
+y = y/100
+
 
 print(X)
 
